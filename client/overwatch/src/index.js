fcb80324a7c3d3f470a40be96321555714e1903b
Jafsari <justinafsari@yahoo.com>
2018-05-16 22:00:48 -0700

    connecting redux

--- client/overwatch/src/index.js
+++ client/overwatch/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware } from 'redux';
+import reducers from './reducers'
+import reduxThunk from 'redux-thunk'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers,{},applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root'));
